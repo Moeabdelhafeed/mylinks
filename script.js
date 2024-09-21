0b00481f841fe1a6975638542a942a43c8b392e5
Mohammadabdelhafeed <almormohammad939@gmail.com>
2024-09-21 16:48:41 +0300

    third

---
+++ script.js
@@ -0,0 +1,20 @@
+function createSnowflake() {
+    let randomani = Math.floor(Math.random() * 3 + 5) ;
+    const snowflake = document.createElement('i');
+    snowflake.classList.add('fas', 'fa-code', 'snowflake'); 
+    snowflake.style.left = Math.random() * window.innerWidth + 'px'; 
+    snowflake.style.animationDuration = randomani + 's'; 
+    snowflake.style.opacity = Math.random(); 
+    snowflake.style.fontSize = Math.random() * 10 + 10 + 'px'; 
+    document.body.appendChild(snowflake);
+
+
+    setTimeout(() => {
+        snowflake.remove();
+    }, randomani * 1000  ); 
+
+
+}
+
+
+setInterval(createSnowflake, 500); 
