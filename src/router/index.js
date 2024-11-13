226b92ef92135eff2301e6de477b1ecf160e9c59
moeabdelhafeed <almormohammad939@gmail.com>
2024-11-13 17:22:57 +0300

    adding a redirect

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      redirect: '/',
+    },
   ],
 })
 
