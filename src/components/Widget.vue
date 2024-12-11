26c7139b7238d13aef4a5893e2bdd9e39877e28d
= <=>
2024-12-11 21:18:59 +0300

    adding the code

---
+++ src/components/Widget.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <div  class="row justify-content-center w-75" data-index="1" style="max-width: 600px; min-width: 350px;">
+              <a :href="widget.url"  class="link-offset-2 link-underline link-underline-opacity-0">
+              <div  id="link" class="d-flex text-center rounded-pill mt-4 text-light align-items-center position-relative"
+                    style="height: 80px;">
+                    <img :src="widget.img" class=" position-absolute p-2 h-100 w-auto img-fluid rounded-circle"/>
+                    <div class="w-50 mx-auto text-center">
+                        <p style="font-size:15px" class=" mb-0 text-wrap">{{ widget.name }}</p>
+
+                    </div>
+                </div>
+            </a> 
+        </div>
+   
+    </div>
+</template>
+
+<script setup>
+
+defineProps({
+    widget: {
+        type: Object,
+        required: false
+    }
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { useUserStore } from '@/stores/User'
+import { animate } from '@/composables/animate';
+import Widget from '@/components/Widget.vue'
+import Icon from '@/components/Icon.vue'
+
+const {user} = useUserStore();
+
+const {globalBeforeEnter, globalEnter} = animate()
+
+    
+const beforeEnter = (el) => {
+    globalBeforeEnter(el, el.dataset.index);
+}
+
+const enter = (el) => {
+    globalEnter(el, el.dataset.index);
+
+}
+
+const widgetsLength = user.widgets.length 
+
+document.title = user.name
+
+</script>
+
+<template>
+  <main>
+
+    <div class="z-3">
+      <div class="container text-center mt-3">
+
+
+  <transition-group
+    appear
+    @before-enter="beforeEnter"
+    @enter="enter"
+            >
+            <div :key="user.id">
+      
+              <img id="img" :src="user.img" width="100px" class="mt-5 rounded-circle img-fluid " alt="Profile Picture" />
+          <h4 class="mt-3 fw-bold text-light">{{ user.name}}</h4>
+          <p class="text-light mb-0"> {{ user.role }}</p>
+        </div>
+
+            <Widget v-for="(widget , index) in user.widgets" :key="index" :widget="widget" :data-index="index + 1" />
+
+            <div key="icons" :data-index=" widgetsLength  + 1" class="row justify-content-center m-auto mt-3 flex-row" style="width: 200px;padding-bottom: 60px;"  >
+                <Icon v-for="(icon , index) in user.icons" :key="index" :icon="icon"  />
+            </div>
+    </transition-group>
+  </div>
+  </div>
+  </main>
+</template>
